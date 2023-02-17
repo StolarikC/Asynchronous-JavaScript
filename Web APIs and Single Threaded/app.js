952f33fa6b2cc07b88299caafceaa8c5ee02d336
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 09:20:00 -0500

    Add files via upload

---
+++ Web APIs and Single Threaded/app.js
@@ -0,0 +1,11 @@
+console.log("Sending request to server...")
+setTimeout(() => {
+    console.log("Here is your data from the server!")
+}, 3000)
+console.log("I AM THE END OF THE FILE!")
+
+// The console will display:
+// "Sending request to server..."
+// "I AM THE END OF THE FILE"
+// "Here is your data from the server!"
+
